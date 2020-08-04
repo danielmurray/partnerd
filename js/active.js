c0842e44dd235f821bdfa03d59883138cd2ad26a
Dan <daniel.phillip.murray@gmail.com>
2020-08-03 22:31:47 -0500

    make geo request https

--- js/active.js
+++ js/active.js
@@ -46,7 +46,7 @@ $('document').ready(function() {
     e.preventDefault();
     const email = $('#email-input2').val();
     const timestamp = Date.now();
-    $.getJSON('http://www.geoplugin.net/json.gp', function(data) {
+    $.getJSON('https://www.geoplugin.net/json.gp', function(data) {
       $.ajax({
         url: url,
         method: "GET",
